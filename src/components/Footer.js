fb1d9181f07300abaafe8f17542d215a29834d15
KadyrMyrzaev <KadyrMyrzaev>
2022-02-09 13:34:35 +0600

    initial

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer id="footer">
+            <div className="container">
+                <h1>footer</h1>
+            </div>
+      </footer>
+    );
+};
+
+export default Footer;
